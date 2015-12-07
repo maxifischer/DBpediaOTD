6c6795126e05c3dc3a905a2e7bfcc2b42d01412d
Maxi Fischer <maxi.fischer@student.hpi.de>
2015-12-07 14:13:14 +0100

    added script to select the html to open in browser

---
+++ DBpediaOTD.py
@@ -0,0 +1,31 @@
+import webbrowser
+import sys
+import DBPedia_On_This_Day
+
+# format should be open <name of html file>
+# or reload <year, month, day>
+
+def main():
+    new = 2
+    try:
+        if "open" in sys.argv:
+            openind = sys.argv.index("open")
+            file = sys.argv[openind + 1]
+            url = "file://C:/Users/Maxi/Documents/SemanticWebJob/DBpediaOTD/Result Pages/" + file
+            #brow = webbrowser.get('firefox')
+            webbrowser.open_new(url)
+        elif "reload" in sys.argv:
+            reloadind = sys.argv.index("reload")
+            year = sys.argv[reloadind + 1]
+            month = sys.argv[reloadind + 2]
+            day = sys.argv[reloadind + 3]
+            date = int(year), int(month), int(day)
+            DBPedia_On_This_Day.main(date)
+    except IndexError:
+        print("no file selected")
+    except IOError:
+        print("no such file")
+
+
+if __name__ == "__main__":
+    main()
